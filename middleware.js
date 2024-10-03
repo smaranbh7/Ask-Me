b29bffc65b660df95261b531096cc6a7cbf86b91
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-10-03 00:53:35 -0400

    clerk-auth

---
+++ middleware.js
@@ -0,0 +1,20 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+
+
+//Protecting the following routes
+const isProtectedRoute = createRouteMatcher ([
+  '/dashboard(.*)', 
+  '/forum(.*)'
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect()
+})
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+};
