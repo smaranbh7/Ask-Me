c9bc1374b24b37c2af31d91777cf6a56f6dd64c0
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-10-04 23:21:44 -0400

    minor  UI work

---
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AddNewInterview() {
+  return (
+    <div>
+      <div className='p-10 border rounded-lg bg-secondary
+       hover:scale-105 hover:shadow-md cursor-pointer transition-all'>
+        <h2 className='text-lg'>+ Add New</h2>
+      </div>
+    </div>
+  )
+}
+
+export default AddNewInterview
--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Smaran</h2>
-      <Button>Smaran</Button>
+     
     </div>
   );
 }
