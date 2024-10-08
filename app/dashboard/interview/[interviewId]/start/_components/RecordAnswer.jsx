57847849303460bbf96293541f689d57cd159d85
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-10-08 01:12:52 -0400

    record ans button

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswer.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswer.jsx
@@ -1,20 +1,24 @@
 import Webcam  from 'react-webcam'
 import React from 'react'
 import Image from 'next/image'
+import { Button } from '../../../../../../components/ui/button';
 
 function RecordAnswer() {
   return (
-    <div className='flex flex-col my-20 justify-center items-center bg-black rounded-lg p-5'>
-      <Image src={'/webcam.png'} width={200} height={200}
-      className='absolute'/>
-      <Webcam
-      mirrored={true}
-      style={{
-        height:300,
-        width:'100%',
-        zIndex:10,
-      }}
-      />
+    <div className='flex items-center justify-center flex-col'>
+        <div className='flex flex-col mt-20 justify-center items-center bg-black rounded-lg p-5'>
+            <Image src={'/webcam.png'} width={200} height={200}
+            className='absolute'/>
+            <Webcam
+            mirrored={true}
+            style={{
+                height:300,
+                width:'100%',
+                zIndex:10,
+            }}
+            />
+        </div>
+        <Button variant='outline' className='my-10'>Record Answer</Button>
     </div>
   )
 }
