daf711fe18131c878200b61440edc618f2ad15c7
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-10-08 00:09:09 -0400

    question setup styling

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function QuestionsSection({mockInterviewQuestion}) {
   return (
     <div className='p-5 border rounded-lg'>
-        <div>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
             {mockInterviewQuestion && mockInterviewQuestion.map((question,index)=>(
-                <h2>Question #{index+1}</h2>
+                <h2 className='p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer '>Question #{index+1}</h2>
             ))}
         </div>
       
