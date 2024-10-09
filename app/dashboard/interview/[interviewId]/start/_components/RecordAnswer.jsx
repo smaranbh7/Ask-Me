9e635170ffaa55a48d69927bf1f743f24b926cad
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-10-09 01:00:31 -0400

    speech to text test code with styled css button update

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswer.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswer.jsx
@@ -1,51 +1,58 @@
-"use client"
-import Webcam  from 'react-webcam'
-import React from 'react'
-import Image from 'next/image'
+"use client";
+import Webcam from 'react-webcam';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
 import { Button } from '../../../../../../components/ui/button';
 import useSpeechToText from 'react-hook-speech-to-text';
+import { Mic } from 'lucide-react';
 
 function RecordAnswer() {
+    const [userAnswer, setUserAnswer] = useState('');
     const {
-      error,
-      interimResult,
-      isRecording,
-      results,
-      startSpeechToText,
-      stopSpeechToText,
-      } = useSpeechToText ({
+        isRecording,
+        results,
+        startSpeechToText,
+        stopSpeechToText,
+    } = useSpeechToText({
         continuous: true,
         useLegacyResults: false
-      });
+    });
 
+    useEffect(() => {
+        results.forEach((result) => {
+            if (result?.transcript) {
+                setUserAnswer(prevAns => prevAns + result.transcript);
+            }
+        });
+    }, [results]);
 
-  return (
-    <div className='flex items-center justify-center flex-col'>
-        <div className='flex flex-col mt-20 justify-center items-center bg-black rounded-lg p-5'>
-            <Image src={'/webcam.png'} width={200} height={200}
-            className='absolute'/>
-            <Webcam
-            mirrored={true}
-            style={{
-                height:300,
-                width:'100%',
-                zIndex:10,
-            }}
-            />
+    return (
+        <div className='flex items-center justify-center flex-col'>
+            <div className='flex flex-col mt-20 justify-center items-center bg-black rounded-lg p-5'>
+                <Image src={'/webcam.png'} width={200} height={200} className='absolute' />
+                <Webcam
+                    mirrored={true}
+                    style={{
+                        height: 300,
+                        width: '100%',
+                        zIndex: 10,
+                    }}
+                />
+            </div>
+            <Button variant='outline' className='my-10'
+                onClick={isRecording ? stopSpeechToText : startSpeechToText}
+            >
+                {isRecording ? (
+                    <h2 className='text-red-600 flrx gap-2'>
+                        <Mic /> Stop Recording
+                    </h2>
+                ) : (
+                    'Record Answer'
+                )}
+            </Button>
+            <Button onClick={() => console.log(userAnswer)}>Show User Answer</Button>
         </div>
-        <Button variant='outline' className='my-10'>Record Answer</Button>
-        <h1>Recording: {isRecording.toString()}</h1>
-      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
-        {isRecording ? 'Stop Recording' : 'Start Recording'}
-      </button>
-      <ul>
-        {results.map((result) => (
-          <li key={result.timestamp}>{result.transcript}</li>
-        ))}
-        {interimResult && <li>{interimResult}</li>}
-      </ul>
-    </div>
-  )
+    );
 }
 
-export default RecordAnswer
+export default RecordAnswer;
