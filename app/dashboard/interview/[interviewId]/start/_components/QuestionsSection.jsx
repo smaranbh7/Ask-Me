dc566e32dd2d2f0a2b73d8ca9d4048681a568bd1
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-10-09 01:11:45 -0400

    text to speech function

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -1,7 +1,16 @@
-import { Lightbulb } from 'lucide-react'
+import { Lightbulb, Volume2 } from 'lucide-react'
 import React from 'react'
 
 function QuestionsSection({mockInterviewQuestion,activeQuestionIndex}) {
+
+    const textToSpeach=(text)=>{
+        if('speechSynthesis' in window){
+            const speech= new SpeechSynthesisUtterance(text);
+            window.speechSynthesis.speak(speech)
+        }else{
+            alert('Sorry, Your browser does not support text to speech')
+        }
+    }
   return mockInterviewQuestion&&(
     <div className='p-5 border rounded-lg my-10'>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
@@ -13,6 +22,7 @@ function QuestionsSection({mockInterviewQuestion,activeQuestionIndex}) {
             ))}
         </div>
         <h2 className='my-5 text-m md:text-lg'>{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>  
+        <Volume2 className='cursor-pointer' onClick={()=>textToSpeach(mockInterviewQuestion[activeQuestionIndex]?.question)} />
         <div className='border rounded-lg p-5 bg-blue-100 mt-20'>
             <h2 className='flex gap-2 items-center text-primary'>
                 <Lightbulb/>
