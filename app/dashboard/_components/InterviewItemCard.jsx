6fe802157cb0f82fe2b649ca4cb571b7062e33ec
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-10-11 00:05:59 -0400

    Feedback button

--- app/dashboard/_components/InterviewItemCard.jsx
+++ app/dashboard/_components/InterviewItemCard.jsx
@@ -10,6 +10,9 @@ function InterviewItemCard({ interview }) {
   const onStart=()=>{
     router.push("/dashboard/interview/"+interview?.mockId);
   }
+  const onFeedback=()=>{
+    router.push("/dashboard/interview/"+interview?.mockId+"/feedback");
+  }
   return (
     <div className="border p-4 mb-4 rounded-lg shadow-sm">
       <h3 className="font-bold text-primary">{interview?.jobPosition}</h3>
@@ -17,7 +20,9 @@ function InterviewItemCard({ interview }) {
       <p className="text-sm text-gray-600">Created At: {(interview.createdAt)}</p>
       <p className="text-sm text-gray-600">Score: {interview.score || 'N/A'}</p>
       <div className='flex justify-between my-2'> 
-        <Button size="sm" variant="outline">Feedback</Button>
+        <Button size="sm" variant="outline"
+        onClick={onFeedback}
+        >Feedback</Button>
         <Button size="sm"
         onClick={onStart}
         >Start</Button>
