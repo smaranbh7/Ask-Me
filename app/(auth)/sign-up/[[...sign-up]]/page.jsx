64d97eb8d75a2f945e67e0c0280067a2d209ce83
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-10-12 00:14:55 -0400

    sign up layout fix

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -19,7 +19,7 @@ export default function Page() {
               </a>
       
               <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-                Welcome To Mock Interview Portal
+                Sign Up To Mock Interview Portal
               </h2>
       
               <p className="mt-4 leading-relaxed text-white/90">
